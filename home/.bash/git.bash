51140f37102392ca69b37bc12f429cfd51c68232
Oliver Fisher <oliverf@shopify.com>
2015-05-07 10:07:12 -0400

    Only install git completion if not already installed.

--- home/.bash/git.bash
+++ home/.bash/git.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# bash completion scripts
-source /usr/local/etc/bash_completion.d/git-completion.bash
-source /usr/local/etc/bash_completion.d/git-prompt.sh
-source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
+# bash completion scripts - if not already installed
+if ! command -v __git_ps1 > /dev/null 2>&1; then
+  source /usr/local/etc/bash_completion.d/git-completion.bash
+  source /usr/local/etc/bash_completion.d/git-prompt.sh
+  source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
+fi
