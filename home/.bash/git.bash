c2c1bdbbccb79f246da370fdd17f70c9ad277113
Oliver Fisher <oliverf@shopify.com>
2015-08-28 15:37:50 -0400

    Arcanist configuration
    Reviewers: oliver-fisher
    
    Reviewed By: oliver-fisher
    
    Differential Revision: https://shopifytest.phacility.com/D4

--- home/.bash/git.bash
+++ home/.bash/git.bash
@@ -6,3 +6,8 @@ if ! command -v __git_ps1 > /dev/null 2>&1; then
   source /usr/local/etc/bash_completion.d/git-prompt.sh
   source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
 fi
+
+# Phabricator Arcanist stuff
+export PATH=$PATH:/Users/oliverf/phacility/arcanist/bin/
+source /Users/oliverf/phacility/arcanist/resources/shell/bash-completion
+
