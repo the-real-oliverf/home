b946dc45658585b39be5606c83db464041e5b763
Oliver Fisher <oliverf@shopify.com>
2015-11-09 15:40:32 -0500

    delete Phabricator

--- home/.bash/git.bash
+++ home/.bash/git.bash
@@ -6,12 +6,3 @@ if ! command -v __git_ps1 > /dev/null 2>&1; then
   source /usr/local/etc/bash_completion.d/git-prompt.sh
   source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
 fi
-
-# TODO(oliver): Delete this since Phabricator is gone
-if ls --version 2> /dev/null | grep -q GNU; then  # not on Mac
-  export PATH=$PATH:/home/vagrant/src/phacility/arcanist/bin/
-  source /home/vagrant/src/phacility/arcanist/resources/shell/bash-completion
-else
-  export PATH=$PATH:/Users/oliverf/phacility/arcanist/bin/
-  source /Users/oliverf/phacility/arcanist/resources/shell/bash-completion
-fi
