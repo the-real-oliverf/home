ef9898245871204f7c635ed1ea5ef72386d3fb87
Oliver Fisher <oliverf@shopify.com>
2016-11-30 10:56:59 -0500

    Fix bash completion on Linux

--- home/.bash/git.bash
+++ home/.bash/git.bash
@@ -2,9 +2,13 @@
 
 # bash completion scripts - if not already installed
 if ! command -v __git_ps1 > /dev/null 2>&1; then
-  source /usr/local/etc/bash_completion.d/git-completion.bash
-  source /usr/local/etc/bash_completion.d/git-prompt.sh
-  source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
+  if ls --version 2> /dev/null | grep -q GNU; then  # not on Mac
+    source /usr/share/bash-completion/completions/git
+    source /etc/bash_completion.d/git-prompt
+  else  # on Mac
+    source /usr/local/etc/bash_completion.d/git-completion.bash
+    source /usr/local/etc/bash_completion.d/git-prompt.sh
+  fi
 fi
 
 # TODO(oliver): Delete this since Phabricator is gone
